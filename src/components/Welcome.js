3f7da759b3c207cf5ab5fc2b26bd61c0635af96d
SMB <sethmichaelbrown@gmail.com>
2019-03-11 12:20:23 -0600

    Changing the background

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, } from 'react-browser-router'
-// import recording from '../media/video/recording.mov'
+import recording from '../media/video/recording.mov'
 import logo from '../media/svgs/text&logo.svg'
 
 import '../styles/welcome.css'
@@ -9,17 +9,23 @@ import '../styles/welcome.css'
 const Welcome = (props) => {
   return (
     <div className="Welcome">
-      <div className="welcome-header">
-        <img src={logo} height={100} width={400} alt="" />
-        <h3>Subheader goes here and is clever</h3>
-      </div>
+      <div className="welcome-layer">
+        <div className="welcome-header">
+
+          <img src={logo} height={100} width={400} alt="" />
+          <h5>Subheader goes here and is clever</h5>
+
+        </div>
 
 
 
-      <div className="welcome-button">
-        <Link to='/bases'>
-          <button type="button" className="btn btn-outline-light btn-lg">Get Started</button>
-        </Link>
+
+
+        <div className="welcome-button">
+          <Link to='/bases'>
+            <button type="button" className="btn btn-outline-light btn-lg">Get Started</button>
+          </Link>
+        </div>
       </div>
 
     </div>
