48fb8cf4db5bb04edaadc35b20373838b78efa4b
SMB <sethmichaelbrown@gmail.com>
2019-03-20 14:19:16 -0600

    Styling 2 3/20 - 14:19

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -187,9 +187,11 @@ class Home extends Component {
               username={this.state.username} />
           </div>
 
-          <div className="col-md-4 mt-5">
+          <div className="col-md-4 my-2 ml-2">
             <Link to='/editor'>
-              <button type="button" onClick={() => this.newBase()} className="btn btn-outline-dark btn-lg">Create New Base</button>
+              <button type="button" onClick={() => this.newBase()} className="btn btn-outline-dark btn-lg new-base-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /><path d="M0 0h24v24H0z" fill="none" /></svg>
+              </button>
             </Link>
           </div>
         </div>
--- src/components/home/ListView.js
+++ src/components/home/ListView.js
@@ -15,94 +15,41 @@ const ListView = (props) => {
 
   return (
     <div className="ListView">
-      <ul className="list-group">
-        {/* <li className="list-group-item list-group-header">
-          <div className="row">
-            <div className="col-md-10">
-              <div className="row">
-                <div className="col-md-3 header-text" onClick={props.sortBy}>
-                  <a id='baseName'>baseName
-                  {props.sortByVal[0] === 'baseName' ? props.sortByVal[1] === 'down' ?
-                      <i className="fa fa-caret-down"></i> : <i className="fa fa-caret-up"></i> : ''}
-                  </a>
-                </div>
-                <div className="col-md-2 header-text" onClick={props.sortBy}>
-                  <a id='codeLanguage'>Language
-                  {props.sortByVal[0] === 'codeLanguage' ? props.sortByVal[1] === 'down' ?
-                      <i className="fa fa-caret-down"></i> : <i className="fa fa-caret-up"></i> : ''}
-                  </a>
-                </div>
-                <div className="col-md-2 header-text" onClick={props.sortBy}>
-                  <a id='modifiedAt'>Last Modified
-                  {props.sortByVal[0] === 'modifiedAt' ? props.sortByVal[1] === 'down' ?
-                      <i className="fa fa-caret-down"></i> : <i className="fa fa-caret-up"></i> : ''}
-                  </a>
-                </div>
-                <div className="col-md-1 item-text">
-                </div>
-                <div className="col-md-2 header-text" onClick={props.sortBy}>
-                  <a id='createdAt'>Created
-                  {props.sortByVal[0] === 'createdAt' ? props.sortByVal[1] === 'down' ?
-                      <i className="fa fa-caret-down"></i> : <i className="fa fa-caret-up"></i> : ''}
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-2">
-              {props.displaySearchBox ?
-                <div className="col-md-12">
-                  <FormControl type="text" placeholder="Search" className="searchBox" onKeyUp={props.search} onBlur={props.backToIcon} />
-                </div>
-                :
-                <div className="col-md-12">
-                  <a onClick={props.showSearchBox} className="float-right">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                      <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-                      <path d="M0 0h24v24H0z" fill="none" />
-                    </svg>
-                  </a>
-                </div>}
-            </div>
-          </div>
-
-        </li> */}
-
-        {props.userBases.length > 0 ?
-          filteredBases.map((base, idx) =>
-            <li className="list-group-item group-item" id={base.id} key={idx}>
-              <div className="row">
-                <LinkContainer to='/editor' onClick={props.selectBaseId} id={base.id}>
-                  <div className="col-md-10">
-                    <div className="row group-item" id={base.id}>
-                      <div className="col-md-3 item-text" id={base.id}>
-                        {base.baseName}
-                      </div>
-                      <div className="col-md-2 item-text" id={base.id}>
-                        {base.codeLanguage}
-                      </div>
-                      <div className="col-md-2 item-text" id={base.id}>
-                        {moment(base.modifiedAt).calendar()}
-                      </div>
-                      <div className="col-md-1 item-text">
-                      </div>
-                      <div className="col-md-2 item-text" id={base.id}>
-                        {moment(base.createdAt).format('L')}
-                      </div>
-                      <div className="col-md-1 item-text">
+      <ul className="list-group" style={{width: '100vw', height: 'auto', overflow: 'auto'}}>
+        <div className="tester">
+          {props.userBases.length > 0 ?
+            filteredBases.map((base, idx) =>
+              <li className="list-group-item group-item" id={base.id} key={idx}>
+                <div className="row">
+                  <LinkContainer to='/editor' onClick={props.selectBaseId} id={base.id}>
+                    <div className="col-md-10">
+                      <div className="row group-item" id={base.id}>
+                        <div className="col-md-3 item-text" id={base.id}>
+                          {base.baseName}
+                        </div>
+                        <div className="col-md-2 item-text" id={base.id}>
+                          {base.codeLanguage}
+                        </div>
+                        <div className="col-md-2 item-text" id={base.id}>
+                          {moment(base.modifiedAt).calendar()}
+                        </div>
+                        <div className="col-md-1 item-text">
+                        </div>
+                        <div className="col-md-2 item-text" id={base.id}>
+                          {moment(base.createdAt).format('L')}
+                        </div>
+                        <div className="col-md-1 item-text">
+                        </div>
                       </div>
                     </div>
+                  </LinkContainer>
+                  <div className="col-md-2">
+                    <i className="fa fa-trash" onClick={props.deleteBase} id={base.id}></i>
                   </div>
-                </LinkContainer>
-                <div className="col-md-2">
-                  <i className="fa fa-trash" onClick={props.deleteBase} id={base.id}></i>
                 </div>
-              </div>
-            </li>
-          )
-          : ''}
-
-
+              </li>
+            ) : ''}
+        </div>
       </ul>
     </div>
   )
